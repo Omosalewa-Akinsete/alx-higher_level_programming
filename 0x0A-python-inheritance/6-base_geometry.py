328a0f397961fefffa99ca3af98676c125624a9a
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-01-08 07:01:54 -0800

    Improve Geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,7 +5,7 @@ Contains the class BaseGeometry
 
 
 class BaseGeometry:
-    """A class with public instance attribute method area"""
+    """A class with public insance method attribute area"""
     def area(self):
         """Raises an exception when called"""
-        raise Exception("area () is not implemented")
+        raise Exception("area() is not implemented")
