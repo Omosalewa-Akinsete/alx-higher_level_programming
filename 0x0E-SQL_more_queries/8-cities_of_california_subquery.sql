70c8dcf04e92c5ef101548e7407d0a89b9ceb764
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-02-14 07:25:16 -0800

    Cities of Califonia

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of Clifonia that can be found in the database hbtn_0d_usa
-SELECT `id`, `name` FROM `cities` WHERE `state_id` = (SELECT `id` FROM `states` WHERE `name` = "Califonia") ORDER BY `id`;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "Califonia") ORDER BY id ASC;
