36aa086fdce3f59199684513c4609d9f471441f8
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-03-11 10:26:07 -0700

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,8 @@ if (isNaN(size)) {
  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
   }
 }
