29f165f6a760e6d69c26cd5db75b9e2cfd5fc6ae
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-03-28 06:21:06 -0700

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,9 +4,9 @@ Takes in a URL and an email,
 sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
+import sys
 import urllib.request
 import urllib.parse
-import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
