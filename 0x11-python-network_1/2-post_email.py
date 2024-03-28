8e2b6b01c3715c18f9166073f1aef9acd601e93e
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-03-28 06:23:01 -0700

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,8 +9,8 @@ import urllib.request
 import urllib.parse
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    data = {'email': email}
-    data_encoded = parse.urlencode(data).encode('ascii')
-    with request.urlopen(url, data=data_encoded) as response:
-        print(response.read().decode('utf-8'))
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
