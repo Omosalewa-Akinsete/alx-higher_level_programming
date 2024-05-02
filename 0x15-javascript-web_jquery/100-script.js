c5095873287defc6c25e9ac783b9caa1f24a1903
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-05-02 15:49:39 -0700

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// Updates the text color of the <header> element to red (#FF0000)
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
